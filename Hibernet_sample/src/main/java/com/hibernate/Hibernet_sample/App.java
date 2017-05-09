adc49887d96f4e92221e4ad15b836bc045027765
swastin <swastin95.sahoo@gmail.com>
2017-05-10 01:36:10 +0530

    commited

---
+++ Hibernet_sample/src/main/java/com/hibernate/Hibernet_sample/App.java
@@ -0,0 +1,28 @@
+package com.hibernate.Hibernet_sample;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Configuration cfg= new Configuration().configure("hibernate.cfg.xml");
+        
+        SessionFactory sf=cfg.buildSessionFactory();
+        Session session=sf.openSession();
+        Transaction t=session.beginTransaction();
+        
+        User user=new User(1, "swastin","swastin95.sahoo","12345");
+        session.save(user);
+        t.commit();
+        session.close();
+       
+    }
+}
