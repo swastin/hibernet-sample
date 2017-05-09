c8f3264a298154a6f5cb589ff60f601a1cf52713
swastin <swastin95.sahoo@gmail.com>
2017-05-10 02:08:46 +0530

    committed

--- Hibernet_sample/src/main/java/com/hibernate/Hibernet_sample/App.java
+++ Hibernet_sample/src/main/java/com/hibernate/Hibernet_sample/App.java
@@ -10,7 +10,6 @@ public class App
     public static void main(String[] args)
     {
         Configuration cfg= new Configuration().configure("hibernate.cfg.xml");
-        
         SessionFactory sf=cfg.buildSessionFactory();
         Session session=sf.openSession();
         Transaction t=session.beginTransaction();
