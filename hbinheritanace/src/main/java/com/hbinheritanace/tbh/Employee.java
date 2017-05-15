3281a26c6c2538787494eb2ce268aa0fd3e68e6b
swastin <swastin95.sahoo@gmail.com>
2017-05-16 00:11:21 +0530

    mx cmx

---
+++ hbinheritanace/src/main/java/com/hbinheritanace/tbh/Employee.java
@@ -0,0 +1,54 @@
+package com.hbinheritanace.tbh;
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)  
+@DiscriminatorColumn(name="type",discriminatorType=DiscriminatorType.STRING)  
+@DiscriminatorValue(value="employee")  
+public class Employee {
+	@Id
+	@GeneratedValue
+	private Long id;
+	private String name;
+	private Integer age;
+	private String gender;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public Long getId() {
+		return id;
+	}
+	/**
+	 * @param id
+	 * @param name
+	 * @param age
+	 * @param gender
+	 */
+	public Employee(String name, Integer age, String gender) {
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+	}
+	/**
+	 * 
+	 */
+	public Employee() {
+	}
+	
+
+}
